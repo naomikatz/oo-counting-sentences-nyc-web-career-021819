fabd39197ecda5d960a1441dd056b3be8727837e
Naomi Katz <naykatz@gmail.com>
2019-03-04 23:01:37 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,35 @@ require 'pry'
 class String
 
   def sentence?
-    
+     self.end_with?(".")
   end
 
   def question?
+    self.end_with?("?")
 
   end
 
   def exclamation?
+     self.end_with?("!")
 
   end
+  
+  def split_sentence
+  self.split.map do |word|
+      word[-1]
+end
 
   def count_sentences
-
+    count = 0
+    array = self.split.map do |word|
+              word[-1]
+            end 
+    array.each do |word|
+      if word.include?("!", "?", ".")
+        count += 1
+      end
+      return count 
+    end
   end
+end 
 end
